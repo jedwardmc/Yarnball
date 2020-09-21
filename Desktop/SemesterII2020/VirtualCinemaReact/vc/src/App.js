85f7183269e24fea31c9399001bfb1a64cfd2bf0
Jasper <jasper.cherry@gmail.com>
2020-09-21 21:22:25 +1000

    Add VC

---
+++ Desktop/SemesterII2020/VirtualCinemaReact/vc/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="AppHeader">
+        <h1>VirtualCinema</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
